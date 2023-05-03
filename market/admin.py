0955fbb4c5344c56aba6ee95d20c7a29f903b9a9
Khamzat Visingiriev <hamzat.visingirievich@gmail.com>
2023-05-03 23:33:13 +0300

    Create "Position" Model

--- market/admin.py
+++ market/admin.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.contrib import admin
 from .models import SliderImage
 
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["name" + 'id']}
+
 
 admin.site.register(SliderImage)
--- market/models.py
+++ market/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
 
+# TODO: Make product model
+
 class SliderImage(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/slider')
     uploaded = models.DateTimeField(auto_now_add='YYYY-MM-DD HH:MM')
     text = models.TextField()
+
+
+class Category(models.Model):
+    pass
+
+
+class Unit(models.Model):
+    pass
+
+
+class Position(models.Model):
+    category = models.ForeignKey(to='Category', on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/products')
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    unit = models.ForeignKey(to='Unit', on_delete=models.DO_NOTHING)
+    ordered_num = models.PositiveIntegerField()
+    slug = models.SlugField()
+    created = models.DateTimeField(auto_now_add="True")
+
